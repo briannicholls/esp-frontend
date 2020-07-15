34fdf3645212f735572b01801b9b430a36287303
Brian Nicholls <nichol88@gmail.com>
2020-07-15 18:49:08 -0400

    add score display modal

--- public/js/index.js
+++ public/js/index.js
@@ -201,11 +201,28 @@ function applyModal() {
   }
 }
 
-function applyImageModal() {
-  let modal = document.getElementById("image-modal");
+function applyScoreModal() {
+  let modal = document.getElementById("score-modal");
+  // let btn = document.getElementById("view-instructions");
+  // Get the <span> element that closes the modal
+  let span = document.getElementsByClassName("close")[1];
+  // When the user clicks on the button, open the modal
+  // btn.onclick = function() {
+  //   modal.style.display = "block";
+  // }
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 }
 
-function applyModals() {applyModal(); applyImageModal();}
+function applyModals() {applyModal(); applyScoreModal();}
 
 // add onClick action to Start Game button
 function addStartGameListener() {
