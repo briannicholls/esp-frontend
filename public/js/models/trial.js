c9f18f78ba85284b33d84249e498ae415ccfa847
Brian (Home) <nichol88@gmail.com>
2020-07-18 02:31:42 -0400

    remove Trial model for now

--- public/js/models/trial.js
+++ public/js/models/trial.js
@@ -1,26 +1,26 @@
-class Trial {
-  constructor() {
-    this.score = 0
-    this.turns = []
-  }
-
-  submit() {
-    fetch(`${API}/trials`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({
-        score: this.score,
-        turns: this.turns
-      })
-    })
-    .then(resp => resp.json())
-    .then(json => {console.log(json)})
-  }
-
-  addTurn(turn) {
-    this.turns.push(turn)
-  }
-}
+// class Trial {
+//   constructor() {
+//     this.score = 0
+//     this.turns = []
+//   }
+//
+//   submit() {
+//     fetch(`${API}/trials`, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Access-Control-Allow-Origin': '*'
+//       },
+//       body: JSON.stringify({
+//         score: this.score,
+//         turns: this.turns
+//       })
+//     })
+//     .then(resp => resp.json())
+//     .then(json => {console.log(json)})
+//   }
+//
+//   addTurn(turn) {
+//     this.turns.push(turn)
+//   }
+// }
