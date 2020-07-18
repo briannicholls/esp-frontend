19a05269f61a5492789451b01cea3ba6c722e97d
Brian (Home) <nichol88@gmail.com>
2020-07-17 21:55:40 -0400

    commit part 2

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,14 @@
-const API = {
-  baseUrl: 'http://localhost:3000'
-}
+const API = {baseUrl: 'http://localhost:3000'}
 
 document.addEventListener('DOMContentLoaded', () => {
   applyModals()
-  addStartGameButton()
+  addStartGameListener()
 })
 
 function handleClick(e) {
   const userChoice = e.target.id
   submitChoice(userChoice)
+  removeGameButtonListeners()
 }
 
 function getGameButtons() {
@@ -35,50 +34,79 @@ function submitChoice(choice) {
   if (turn.user_choice === `${turn.computer_choice}`) {
     console.log("correct!")
     // show image modal
-    getImageModal().style.display = 'block'
+    toggleImageModal()
     // play "correct" sound
     getCorrectSound().play()
+    incrementTurnCounter()
+    continueGame(4600)
   } else if (choice == 'pass') {
     console.log('user passed')
+    highlightButton(turn.computer_choice)
+    continueGame(1500)
   } else {
     console.log('incorrect!')
+    // highlight correct choice
+    highlightButton(turn.computer_choice)
     // play "incorrect" sound
-  }
-
-  function getCorrectSound() {
-    return document.getElementById('correct-sound')
-  }
-
-  const totalChoicesMadeByUser = Turn.all.reduce((total, turn) => {
-    if (turn.user_choice != 'pass') {
-      return total += 1
-    } else {
-      return total
-    }
-  }, 0)
-
-  if (totalChoicesMadeByUser < 24) {
-    beginNewTurn()
-  } else {
-    endGame()
+    getIncorrectSound().play()
+    incrementTurnCounter()
+    continueGame(1500)
   }
 }
 
-function endGame() {
-  removeGameButtonListeners()
-
-  const result = Turn.all.reduce((total, turn) => {
-    if (turn.user_choice == turn.computer_choice) {
-      total.correct += 1
-    } else if (turn.user_choice != 'pass'){
-      total.incorrect += 1
+function continueGame(delay) {
+  setTimeout(() => {
+    if (gameIsOver()) {
+      return gameOver()
     } else {
-
+      beginNewTurn()
     }
-    return total
-  }, {correct: 0, incorrect: 0})
-  console.log(result)
-  // game is over, submit data to server
+  }, delay)
+}
+
+function highlightButton(id) {
+  const element = document.getElementById(id)
+  element.classList.add('w3-white')
+}
+
+function getButton(id) {
+  return document.getElementById(id)
+}
+
+function toggleImageModal() {
+  const modal = getImageModal()
+  if (modal.style.display == 'block') {
+    modal.style.display = 'none'
+  } else {
+    modal.style.display = 'block'
+  }
+}
+
+function showImageModal() {
+  const modal = getImageModal()
+  if (modal.style.display == 'none') {
+    modal.style.display = 'block'
+  }
+}
+
+function hideImageModal() {
+  const modal = getImageModal()
+  if (modal.style.display == 'block') {
+    modal.style.display = 'none'
+  }
+}
+
+function gameIsOver() {
+  const totalChoicesMadeByUser = parseInt(document.getElementById('turn-counter').innerText)
+  return totalChoicesMadeByUser >= 24 ? true : false
+}
+
+function getCorrectSound() {
+  return document.getElementById('correct-sound')
+}
+
+function getIncorrectSound() {
+  return document.getElementById('incorrect-sound')
 }
 
 // Game Loop
@@ -88,9 +116,48 @@ function startGame() {
 }
 
 function beginNewTurn() {
-  const url = 'https://previews.123rf.com/images/vitplus/vitplus1612/vitplus161200019/69533061-little-puppy-sleeping-sweet-on-soft-cozy-knitted-sweater-cute-dog-dreaming-retro-filtered.jpg'
-  new Turn({image_url: url})
-  addImageToDOM(url)
+  // get image from API!
+  fetch('https://picsum.photos/700')
+    .then(resp => resp.url)
+    .then(url => {
+      new Turn({image_url: url})
+      hideImageModal()
+      // reset highlighted square
+      removeHighlightFromButton()
+      addImageToDOM(url)
+      console.log('added image to DOM')
+      // add click listeners back to btns
+      addGameButtonListeners()
+    })
+}
+
+function gameOver() {
+  console.log("Game Over")
+  hideImageModal()
+  removeHighlightFromButton()
+  removeGameButtonListeners()
+  resetTurnCounter()
+  displayScore()
+}
+
+function displayScore() {
+  const result = Turn.all.reduce((total, turn) => {
+    if (turn.user_choice == turn.computer_choice) {
+      total.correct += 1
+    } else if (turn.user_choice != 'pass'){
+      total.incorrect += 1
+    } else {
+    }
+    return total
+  }, {correct: 0, incorrect: 0})
+  console.log(result)
+  document.getElementById('score-modal').style.display = 'block'
+  document.getElementById('score-display').innerText = `You scored ${result.correct} out of 24 (${Math.round((result.correct / 24) * 100)}%)`
+}
+
+function removeHighlightFromButton() {
+  const btn = document.getElementsByClassName('w3-white')[0]
+  !!btn ? btn.classList.remove('w3-white') : null
 }
 
 function getImageModal() {
@@ -98,7 +165,6 @@ function getImageModal() {
 }
 
 function addImageToDOM(url) {
-  // getImageModal().style.display = 'none'
   const imageElement = document.getElementById('image-display')
   imageElement.setAttribute('src', url)
   imageElement.setAttribute('alt', 'description')
@@ -137,17 +203,31 @@ function applyModal() {
   }
 }
 
-function applyImageModal() {
-  let modal = document.getElementById("image-modal");
+function applyScoreModal() {
+  let modal = document.getElementById("score-modal");
+  // let btn = document.getElementById("view-instructions");
+  // Get the <span> element that closes the modal
   let span = document.getElementsByClassName("close")[1];
-  span.onclick = function() { modal.style.display = "none" }
-  window.onclick = event => {if (event.target == modal) {modal.style.display = "none"};};
+  // When the user clicks on the button, open the modal
+  // btn.onclick = function() {
+  //   modal.style.display = "block";
+  // }
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 }
 
-function applyModals() {applyModal(); applyImageModal();}
+function applyModals() {applyModal(); applyScoreModal();}
 
 // add onClick action to Start Game button
-function addStartGameButton() {
+function addStartGameListener() {
   const button = document.getElementById('start_game')
   button.addEventListener('click', () => {
     button.disabled = true
